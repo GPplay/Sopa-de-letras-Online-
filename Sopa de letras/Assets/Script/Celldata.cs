285af68fbf92ab2de38fbe81d53ef246b2746ff9
GPplay <gybramdali2002@hotmail.com>
2025-02-01 15:50:53 -0500

    sopa de letras

--- Sopa de letras/Assets/Script/Celldata.cs
+++ Sopa de letras/Assets/Script/Celldata.cs
@@ -1,13 +1,20 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class CellData : MonoBehaviour
 {
     public int x { get; private set; }
     public int y { get; private set; }
+    public UnityEvent onCellSelected; // Evento para la selección de celdas  
 
     public void SetCoordinates(int x, int y)
     {
         this.x = x;
         this.y = y;
+    }
+
+    public void SelectCell()
+    {
+        onCellSelected.Invoke(); // Llama al evento al seleccionar la celda  
     }
 }
